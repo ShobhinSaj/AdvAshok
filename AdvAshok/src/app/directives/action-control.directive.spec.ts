0bd1a91c70bd3bd214c0c15aca463ab0f665bc44
madmax117 <maxjonas117@gmail.com>
2024-03-25 02:12:49 -0400

    mvp deployed to githubpages

--- AdvAshok/src/app/directives/action-control.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ActionControlDirective } from './action-control.directive';
-
-describe('ActionControlDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ActionControlDirective();
-    expect(directive).toBeTruthy();
-  });
-});
