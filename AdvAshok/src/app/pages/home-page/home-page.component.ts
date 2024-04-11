2b95dfd269911cfbfb686539668e086bd262b38b
madmax117 <maxjonas117@gmail.com>
2024-04-10 20:29:01 -0400

    Dislaimer link added to navbar

--- AdvAshok/src/app/pages/home-page/home-page.component.ts
+++ AdvAshok/src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
-import { Component,Inject } from '@angular/core';
-import { CommonModule,DOCUMENT } from '@angular/common';
+import { Component} from '@angular/core';
+import { CommonModule} from '@angular/common';
 import { SliderComponentComponent } from '../../components/slider-component/slider-component.component'
 import { Router } from '@angular/router';
 import { DisclaimerPageComponent } from '../disclaimer-page/disclaimer-page.component'
@@ -12,24 +12,15 @@ import { DisclaimerPageComponent } from '../disclaimer-page/disclaimer-page.comp
   styleUrls: ['./home-page.component.scss','../../../styles.scss']
 })
 export class HomePageComponent{
-
-  visitedBefore:boolean=false;
-  constructor(@Inject(DOCUMENT) private document: Document, private router: Router){
-    if (typeof window !== 'undefined') {
-      const localStorage = document.defaultView?.localStorage;
-      const visitedBefore = localStorage.getItem('visitedBefore');
-      this.visitedBefore = visitedBefore !== null ? JSON.parse(visitedBefore) : false;
-      }
-  }
+  constructor(private router: Router){}
+ 
  
   OpenContact(){
     // this.router.navigate(['/contact #contact_info']);
     this.router.navigate(['/contact', { fragment: 'contact_info' }]);
   }
-  
   OpenServices(){
     this.router.navigate(['/services']);
   }
-  
 }
 
